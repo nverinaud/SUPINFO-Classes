262f334411e3e1a2947199cc829f960b6c267230
Nicolas VERINAUD <n.verinaud@gmail.com>
2013-01-10 13:16:47 +0100

    Update C Conditionnels & Iterations.

--- C/Conditionnels/Conditionnels/main.c
+++ C/Conditionnels/Conditionnels/main.c
@@ -10,113 +10,52 @@
 
 int main(int argc, const char * argv[])
 {
-	double x;
-	int n = 0;
-	int neg = 1;
-	
-	scanf("%lf", &x);
-	
-	neg = (x < 0) ? -1 : 1;
-//	x = (x < 0) ? -x : x;
-	x *= neg;
-	
-	while (n+1 <= x)
+	// Note avec if...else if...else
+	double note;
+	scanf("%lf", &note);
+
+	if (note < 8)
 	{
-		n++;
+		printf("Recalé\n");
+	}
+	else
+	{
+		if (note < 10)
+		{
+			printf("Rattrapages\n");
+		}
+		else
+		{
+			printf("Admins !\n");
+		}
+	}
+
+	// Note avec switch
+	int noteAsInt = note;
+	switch (noteAsInt) // On ne peut pas faire de switch sur un double, il faut le faire sur un int
+	{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+			printf("Recalé\n");
+			break;
+				   
+		case 8:
+		case 9:
+			printf("Rattrapages!\n");
+			break;
+			
+		default:
+			printf("Admis !\n");
+			break;
 	}
 	
-	x *= neg;
-	n *= neg;
-	
-	printf("La partie entière de %f vaut %d", x, n);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	double note;
-//	scanf("%lf", &note);
-//	
-//	if (note < 8)
-//	{
-//		printf("Recalé\n");
-//	}
-//	else
-//	{
-//		if (note < 10)
-//		{
-//			printf("Rattrapages\n");
-//		}
-//		else
-//		{
-//			printf("Admins !\n");
-//		}
-//	}
-	
-//	switch ((int)note)
-//	{
-//		case 0:
-//		case 1:
-//		case 2:
-//		case 3:
-//		case 4:
-//		case 5:
-//		case 6:
-//		case 7:
-//			printf("Recalé\");
-//			break;
-//				   
-//	    case 8:
-//		case 9:
-//			printf("Rattrapages!\n");
-//			break;
-//			
-//		default:
-//			printf("Admis !\n");
-//			break;
-//	}
-	
-	
-//	int n = 2;
-//	
-//	switch (n)
-//	{
-//		case 1:
-//		{
-//			printf("n vaut 1\n");
-//			printf("n vaut 1 c'est sûre !\n");
-//			break;
-//		}
-//			
-//		case 2:
-//			printf("n vaut 2\n");
-//			break;
-//			
-//		case 3:
-//			printf("n vaut 3\n");
-//			break;
-//			
-//		default:
-//			printf("n ne vaut ni 1 ni 2 ni 3\n");
-//			break;
-//	}
-	
-	
-	
-	
-    return 0;
+	return 0;
 }
 
 
--- C/Iterations/Iterations/main.c
+++ C/Iterations/Iterations/main.c
@@ -3,6 +3,7 @@
 
 int main(int argc, const char * argv[])
 {
+	// Table de multiplications
 	int i, j;
 	
 	for (i = 1; i <= 10; i++)
@@ -13,6 +14,27 @@ int main(int argc, const char * argv[])
 			printf("%d x %d = %d\n", i, j, i*j);
 		}
 	}
+	
+	// Partie entière
+	double x;
+	int n = 0;
+	int neg = 1;
+	
+	scanf("%lf", &x);
+	
+	neg = (x < 0) ? -1 : 1;
+//	x = (x < 0) ? -x : x; // équivalent à x *= neg
+	x *= neg;
+	
+	while (n+1 <= x)
+	{
+		n++;
+	}
+	
+	x *= neg;
+	n *= neg;
+	
+	printf("La partie entière de %f vaut %d", x, n);
 	
     return 0;
 }
