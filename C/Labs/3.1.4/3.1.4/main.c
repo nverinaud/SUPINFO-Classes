cc042bed8ee6702bee384238b4b9a076a99b509c
Nicolas VERINAUD <n.verinaud@gmail.com>
2013-01-14 15:55:10 +0100

    Add labs 3.1.4

---
+++ C/Labs/3.1.4/3.1.4/main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  3.1.4
+//
+//  Created by Nicolas VERINAUD on 14/01/13.
+//  Copyright (c) 2013 Nicolas VERINAUD. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+	int nombreDeLignes, nombreEtoiles, i;
+	
+	printf("Entrer un nombre");
+	scanf("%d", &nombreDeLignes);
+	
+	while (nombreDeLignes<0)
+	{
+		printf("Entrez un entier positif");
+		scanf("%d", &nombreDeLignes);
+	}
+	
+	nombreEtoiles=nombreDeLignes;
+	
+	
+	for (i=nombreDeLignes; i>=1; i--)
+	{
+		for (nombreEtoiles=1; nombreEtoiles<=i; nombreEtoiles++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+    return 0;
+}
+
