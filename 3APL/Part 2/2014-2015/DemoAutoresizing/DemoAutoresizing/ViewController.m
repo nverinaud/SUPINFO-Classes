fd3929587b6dafb3a7eaeb6f153ec774734ff7d3
Nicolas VERINAUD <n.verinaud@gmail.com>
2015-03-04 11:45:06 +0100

    Add demo auto layout 2015.

---
+++ 3APL/Part 2/2014-2015/DemoAutoresizing/DemoAutoresizing/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  DemoAutoresizing
+//
+//  Created by Nicolas VERINAUD on 04/03/15.
+//  Copyright (c) 2015 Nicolas VERINAUD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
